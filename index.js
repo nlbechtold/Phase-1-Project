7c67fe07573cbe6300f3c30bbc71f48c0828325c
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-18 10:50:02 -0600

    index.js most recent 6.18.24

--- index.js
+++ index.js
@@ -1,87 +1,59 @@
-fetch("https://yoga-api-nzy4.onrender.com/v1/poses")
+fetch("https://yoga-api-nzy4.onrender.com/v1/categories")
 .then(r => r.json())
-.then(poses =>{
+.then(categories =>{
+function displayEach(input){
+ categories.forEach((category)=>{
+  // console.log(categorie.poses)
 
- poses.forEach((pose)=>{
+//gets all the categories of the poses like all the core ones 
+console.log(category)
+    console.log(input)
+    // const panel = document.querySelector("#pose-display-image")
+    const fullDisplay = document.querySelector("#pose-display-image")
+      // fullDisplay.innerHTML = ""
+    category.poses.forEach((categoriesPoses)=>{
+    if (categoriesPoses.category_name === input ){
+      console.log("UrMom")
+        console.log(fullDisplay)
+        const name = document.createElement("h2")
+        const image = document.createElement("img")
+        const benefits = document.createElement("h3")
+        const button = document.createElement("button")
 
+        button.textContent = "Show Me How"
+        name.textContent = categoriesPoses.english_name
+        image.src = categoriesPoses.url_png
+        benefits.textContent = categoriesPoses.pose_benefits
 
-      console.log(pose)
-     displayPose(pose)
+        fullDisplay.append(name, image, benefits, button)
+
+        button.addEventListener("click", () => {
+            const textHowtoSel = document.querySelector("#hover-text")
+            const textHowTo = document.createElement("p")
+            
+            textHowTo.textContent = categoriesPoses.pose_description
+            button.append(textHowTo)
+            console.log (textHowTo.textContent)
+            
+              }
+        
+    )
+
+        
+        
+    }
 
     })
-    
   })
-// function displayHowTo = {
-//     const textHowto = fullDisplay.querySelector
-// }
-
-function displayPose(pose) {
-    const fullDisplay = document.querySelector("#pose-display-image")
-
-    const name = document.createElement("h2")
-    const image = document.createElement("img")
-    const benefits = document.createElement("h3")
-    
-    name.textContent = pose.english_name
-    image.src = pose.url_png
-    benefits.textContent = pose.pose_benefits
-
-    fullDisplay.append(name, image, benefits)
 }
 
-        
-    image.addEventListener("mouseover", () => {
-        const textHowtoSel = document.querySelector("#hover-text")
-        const textHowTo = document.createElement("h4")
-        
-        textHowTo.textContent = pose.pose_description
-        textHowtoSel.append(textHowTo)
-        console.log (textHowTo)
-            document.getElementById("hover-text").style.display = 'block';
-          }
-    
-)
-    image.getElementById("hover-text").addEventListener("mouseout", () => {
-
-            document.getElementById("hover-text").style.display = 'none';
-          }
-
-)
-    
-  
+  const dropdownBtn = document.querySelector("#tweleve-options")
+  dropdownBtn.addEventListener("change", function (e) {
+    const fullDisplay = document.querySelector("#pose-display-image")
+      fullDisplay.innerText = ""
+      console.log("Trigger")
+    displayEach(e.target.value)
+  })
 
 
-
-//     image.addEventListener("mouseout", () => {
-//         textHowto.textContent= 'none';
-//     }
-//     )
-//     }
-
-
-
-    
-// function displayHowTo(poseHow) {
-//             const textHowtoSel = document.querySelector(".gallery")
-//             const textHowTo = document.createElement("h4")
-
-//             textHowTo.textContent = pose.pose_description
-            
-//             console.log(poseHow)
-//             textHowtoSel.append(textHowTo)
-//             console.log(poseHow)
-
-//             fullDisplay.addEventListener("click", () => {
-//                 displayHowTo(poseHow)
-//                 console.log(poseHow)
-                
-//             }
-//             )
-//         }
-
-   
-
-
-// https://yoga-api-nzy4.onrender.com/v1/categories?id=5&level=beginner
-
-// "https://yoga-api-nzy4.onrender.com/v1/poses"
+  })
